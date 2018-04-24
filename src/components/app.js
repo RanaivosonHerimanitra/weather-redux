c1df0cb611e00aa49950b9cb7c890bf850fd31f3
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-04-24 14:26:20 -0400

    searchbar is the component that must have constructor because it is receiving state

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,7 @@ import React , {Component} from "react"
 import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {term:""}
-    }
+    
     render() {
         return (
             <div>
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,19 @@
 import React , {Component} from "react"
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {term:""}
+    }
     render() {
         return (
             <form className="input-group">
-              <input />
+              <input 
+                 placeholder="get a 5 day forecast in your favorite city"
+                 className="form-control"
+                 value ={this.state.term}
+                 onChange={this.onInputChange}
+                 />
               <span className="input-group-btn">
               <button type="submit" className="btn btn-secondary">submit</button>
               </span>
