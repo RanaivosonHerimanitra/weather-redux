b0ec6b80d880d3abcca64b5c4045a1244647ad27
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2018-04-25 00:25:41 -0400

    setting up my action get request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,12 @@
+import axios from "axios"
 const API_KEY="398eaa6647e9a1da45cc18709f53afe5"
-//middleware modifieur d action
+const ROUTE_URL=`http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+//middleware modifieur/intercepteur d'actions
+export const FETCH_WEATHER ="FETCH_WEATHER"
+export function fetchWeather(city,countrycode) {
+    const query_url = `${ROUTE_URL}&q=${city},${countrycode}`
+    const request = axios.get(query_url)
+    return {
+        type:FETCH_WEATHER
+    }
+}
